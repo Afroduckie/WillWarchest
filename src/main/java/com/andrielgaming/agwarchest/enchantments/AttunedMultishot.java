1f01d9f59ad5dc077f5ac814bd8bf7a2e2d93b87
Andrew <wak160130@utdallas.edu>
2020-07-13 21:01:16 -0500

    Update AttunedMultishot.java
    Forwarding most recent changes, still non functional.
    
    Successfully grabs crossbow event but nullifies all noise and ammo, essentially making the crossbow useless when enchanted

--- src/main/java/com/andrielgaming/agwarchest/enchantments/AttunedMultishot.java
+++ src/main/java/com/andrielgaming/agwarchest/enchantments/AttunedMultishot.java
@@ -1,5 +1,7 @@
 package com.andrielgaming.agwarchest.enchantments;
 
+import java.util.Random;
+
 import com.andrielgaming.agwarchest.WarchestMaster;
 import com.andrielgaming.agwarchest.init.EnchantInit;
 
@@ -9,16 +11,21 @@ import net.minecraft.enchantment.EnchantmentType;
 import net.minecraft.enchantment.Enchantments;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.item.CrossbowItem;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.nbt.ListNBT;
 import net.minecraft.world.World;
-import net.minecraftforge.event.entity.player.ArrowLooseEvent;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent.RightClickItem;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
 
 public class AttunedMultishot extends Enchantment 
 {
+	private static final Random rand = new Random();
+	
 	   public AttunedMultishot(Enchantment.Rarity p_i50017_1_, EquipmentSlotType... p_i50017_2_) 
 	   {
 	      super(p_i50017_1_, EnchantmentType.CROSSBOW, p_i50017_2_);
@@ -44,21 +51,47 @@ public class AttunedMultishot extends Enchantment
 	      return super.canApplyTogether(ench) && ench != Enchantments.PIERCING;
 	   }
 	   
+	   //Uses the event subscriber bus to attempt to grab the crossbow item being used on
+	   // occurrence of a RightClickItem event, meaning when the player tries to shoot.
 	   @Mod.EventBusSubscriber(modid = WarchestMaster.MOD_ID, bus = Bus.FORGE)
-	   public static class Attuned 
-		{
+	   public static class AttunedMultishotEquipped
+	   {
 			@SubscribeEvent
-			public static void doStuff(ArrowLooseEvent event) 
+			@SuppressWarnings("static-access")
+			public static void doStuff(RightClickItem event) 
 			{
 				//Grab reference to player, world, and the bow used
 				PlayerEntity player = event.getPlayer();
 				World world = player.world;
-				ItemStack bow = event.getBow();
-				
-				//Check if its a crossbow specifically and has the attuned multishot enchant
+				ItemStack bow = event.getItemStack();
+			
+				//Checks if bow loaded, adds up to 10 additional arrows
 				if(bow.getItem() == Items.CROSSBOW && bow.isEnchanted() && EnchantmentHelper.getEnchantmentLevel(EnchantInit.ATTUNED_MULTISHOT.get(), bow) > 0)
 				{
-					
+					 CrossbowItem temp = (CrossbowItem)bow.getItem();
+					 if(temp.isCharged(bow))
+					 {
+						 CompoundNBT compoundnbt = bow.getOrCreateTag();
+					     ListNBT listnbt;
+					     if(compoundnbt.contains("ChargedProjectiles", 9)) 
+					     {
+					        listnbt = compoundnbt.getList("ChargedProjectiles", 10);
+					     } 
+					     else 
+					     {
+					        listnbt = new ListNBT();
+					     }
+					     for(int x = 0;x<8;x++)
+					     {
+					    	 if(rand.nextInt(9)>0)
+					    	 {
+					    		 CompoundNBT compoundnbt1 = new CompoundNBT();
+							     bow.write(compoundnbt1);
+					    		 listnbt.add(compoundnbt1); 
+					    	 } 
+					     }
+					     compoundnbt.put("ChargedProjectiles", listnbt);
+					 }
 				}
 			}
 		}
