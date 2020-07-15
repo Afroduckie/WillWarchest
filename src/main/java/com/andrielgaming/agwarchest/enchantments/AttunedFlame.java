f7b17728b54a0b429f2bce6af4e028be7cda42b8
Andrew <wak160130@utdallas.edu>
2020-07-14 21:56:49 -0500

    Update AttunedFlame.java
    still broken lol

--- src/main/java/com/andrielgaming/agwarchest/enchantments/AttunedFlame.java
+++ src/main/java/com/andrielgaming/agwarchest/enchantments/AttunedFlame.java
@@ -3,12 +3,15 @@ package com.andrielgaming.agwarchest.enchantments;
 import com.andrielgaming.agwarchest.WarchestMaster;
 import com.andrielgaming.agwarchest.init.EnchantInit;
 
+import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
+import net.minecraft.block.BushBlock;
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.enchantment.EnchantmentType;
-import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.projectile.AbstractArrowEntity;
+import net.minecraft.entity.projectile.ArrowEntity;
 import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.BowItem;
 import net.minecraft.item.ItemStack;
@@ -18,8 +21,8 @@ import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.world.World;
+import net.minecraftforge.event.entity.ProjectileImpactEvent;
 import net.minecraftforge.event.entity.living.LivingHurtEvent;
-import net.minecraftforge.event.entity.player.ArrowLooseEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
@@ -92,43 +95,62 @@ public class AttunedFlame extends Enchantment
 	{
 		return false;
 	}
-	//BowItem
+	
 	@Mod.EventBusSubscriber(modid = WarchestMaster.MOD_ID, bus = Bus.FORGE)
 	public static class AttunedFlameEquipped
 	{
-		//Flame II sets the enemy on fire with Soul Fire instead of regular fire
 		private static PlayerEntity player;
-		private static BowItem bow;
-		private static ItemStack bowstack;
+		private static boolean pass = false;
 		
-		//I'm not sure what event to use, so I'm putting a few functions down here 
-		
-		@SubscribeEvent
-		public static void doStuff(ArrowLooseEvent event)
-		{
-			player = (PlayerEntity)event.getEntityLiving();
-			bow = (BowItem)event.getBow().getItem();
-			bowstack = event.getBow();
-		}
-		
+		// Flame II sets the enemy on fire with Soul Fire instead of regular fire
 		@SubscribeEvent
 		public static void doStuff(LivingHurtEvent event)
 		{
-			if(event.getSource().isProjectile() && event.getSource().getTrueSource() == player)
+			if(event.getSource().isProjectile() && event.getSource().getTrueSource() instanceof PlayerEntity)
 			{
-				ItemStack bow2 = player.getActiveItemStack();
-				if(bow2.equals(bowstack))
+				player = (PlayerEntity)event.getSource().getTrueSource();
+				ItemStack bowstack = player.getHeldItemMainhand();
+				if(bowstack.getItem() instanceof BowItem && bowstack.isEnchanted()	&& EnchantmentHelper.getEnchantments(bowstack).containsKey(EnchantInit.ATTUNED_FLAME.get()))
 				{
-					if(bowstack.isEnchanted() && EnchantmentHelper.getEnchantments(bowstack).containsKey(EnchantInit.ATTUNED_FLAME.get()))
+					pass = true;
+				}
+			}
+		}//Blocks
+		
+		@SubscribeEvent
+		public static void doStuff(ProjectileImpactEvent event)
+		{
+			if(pass && player != null && event.getEntity() instanceof ArrowEntity)
+			{
+				AbstractArrowEntity arr = (AbstractArrowEntity)event.getEntity();
+				arr.setFire(9999);
+				World world = arr.world;
+				for(int i = 0;i<100;i++)
+				{
+					BlockPos rand = new BlockPos(arr.getPosXRandom(5), arr.getPosY(), arr.getPosZRandom(5));
+					BlockState stat = world.getBlockState(rand);
+					if(stat.getBlock() == Blocks.AIR)
 					{
-						LivingEntity shot = (LivingEntity)event.getEntity();
-						World world = shot.world;
-						world.setBlockState(new BlockPos(shot.getPosX(), shot.getPosY(), shot.getPosZ()), Blocks.field_235335_bO_.getDefaultState());
-						//world.setBlockState(new BlockPos(shot.getPosX(), shot.getPosY(), shot.getPosZ()), Blocks.AIR.getDefaultState());
+						//Look at next 3 blocks down
+						BlockState d1 = world.getBlockState(rand.down());
+						BlockState d2 = world.getBlockState(rand.down().down());
+						BlockState d3 = world.getBlockState(rand.down().down().down());
+						
+						if(d1.getBlock() != Blocks.AIR && !(d1.getBlock() instanceof BushBlock))
+						{//AbstractBlock
+							world.setBlockState(rand, Blocks.field_235335_bO_.getDefaultState());
+						}
+						if(d2.getBlock() != Blocks.AIR && !(d2.getBlock() instanceof BushBlock))
+						{
+							world.setBlockState(rand.down(), Blocks.field_235335_bO_.getDefaultState());
+						}
+						if(d3.getBlock() != Blocks.AIR && !(d3.getBlock() instanceof BushBlock))
+						{
+							world.setBlockState(rand.down().down(), Blocks.field_235335_bO_.getDefaultState());
+						}
 					}
 				}
 			}
 		}
-		
 	}
 }
